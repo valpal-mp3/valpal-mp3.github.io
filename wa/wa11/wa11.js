7277e80f8203bd97ca7b584e7ee832a23c7b7f0d
valpal-mp3 <valerie.nguyen@colorado.edu>
2025-04-06 18:11:46 -0600

    added wa11

--- wa/wa11/wa11.js
+++ wa/wa11/wa11.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const images = ['horse1.jpg', 'horse2.jpg', 'horse3.jpg', 'horse4.jpg', 'horse5.jpg'];
+const images = ['horse1.JPG', 'horse2.JPG', 'horse3.JPG', 'horse4.JPG', 'horse5.JPG'];
 
 /* Declaring the alternative text for each image file */
 const altTxt = ['Horse with apple 1', 'Horse with apple 2', 'Horse with apple 3', 'Horse with apple 4', 'Horse with apple 5'];
